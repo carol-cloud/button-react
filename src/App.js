3b443118501559f194580b95a5e34d3799264761
Ana Carolina <karols.15@hotmail.com>
2020-08-13 01:57:36 -0300

    create button with React

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Counter from './components/counter/counter';
+
+export default class App extends Component {
+  render() {
+    return <Counter />;
+  }
+}
